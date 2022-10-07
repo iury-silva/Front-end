5c2f3a16ad1b33435b9b5e4823b3018df3100af0
iury <iury2002silva@hotmail.com>
2022-10-06 22:24:02 -0300

    first commit

---
+++ javascript/Exercicios/parte02/script.js
@@ -0,0 +1,43 @@
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var foto = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+var min = data.getMinutes()
+var sec = data.getSeconds()
+
+msg.innerHTML = (`Agora são ${hora}:${min}:${sec}`)
+
+if(hora >= 0 && hora < 12){
+//BOM DIA!!
+  foto.src ="manha.jpg"
+  document.body.style.background = '#39546c'
+  document.querySelector("#msg").style.background = '#1a354a'
+  document.querySelector("#msg").style.color = 'white'
+
+}else if(hora >= 12 && hora < 18){
+//BOA TARDE!!
+
+  foto.src ="fototarde.jpg"
+  document.body.style.background = '#89896f'
+  document.querySelector("#msg").style.background = '#6d5843'
+  document.querySelector("#msg").style.color = 'white'
+
+
+}else{
+  //BOA NOITE!!
+
+  foto.src ="noite.jpg"
+  document.body.style.background = '#121f24'
+  document.querySelector('#msg').style.background = "#06151a"
+  document.querySelector('#msg').style.color = 'white'
+
+}
+
+}
+
+function realTime(){
+  setInterval( carregar,1000)
+
+}
---
+++ javascript/aula16/function.js
@@ -0,0 +1,14 @@
+function paraimp(num){
+
+  if(num%2==0){
+
+    return console.log(`O numero ${num} é par!!`)
+  }else{
+
+    return console.log(`O numero ${num} é impar`)
+  }
+
+
+}
+
+let num =paraimp(6)
